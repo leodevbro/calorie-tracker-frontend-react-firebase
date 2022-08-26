74c108da4b6f9ef0549f16b18afb6dbe9ec6caeb
Levan Katsadze <leodevbro@gmail.com>
2022-08-26 19:54:09 +0400

    minor amend of Sweet date time picker

--- src/components/SweetDateTimePicker/SweetDateTimePicker.tsx
+++ src/components/SweetDateTimePicker/SweetDateTimePicker.tsx
@@ -1,14 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { cla } from "src/App";
 // import { Link } from "react-router-dom";
 // import { useAppSelector } from "../app/hooks";
 import style from "./SweetDateTimePicker.module.scss";
 
+import "react-datepicker/dist/react-datepicker.css";
+
 export const SweetDateTimePicker: React.FC<{ className?: string }> = ({ className }) => {
-    return (
-        <div className={cla(className, style.ground)}>
-            Hello-------------------------World
-        </div>
-    );
+  const [startDate, setStartDate] = useState(new Date());
+
+  return <div className={cla(className, style.ground)}>picker</div>;
 };
