acabfdc20a7596eafde2e8c6ab25808d8d08ef23
Levan Katsadze <leodevbro@gmail.com>
2022-08-28 23:06:49 +0400

    update profile page

--- src/pages/ProfilePage/ProfilePage.tsx
+++ src/pages/ProfilePage/ProfilePage.tsx
@@ -8,6 +8,9 @@ import { useAppSelector } from "src/app/hooks";
 import { dbApi } from "src/connection-to-backend/db/bridge";
 import { equalFnForCurrUserDocChange } from "src/App";
 
+import Table from "react-bootstrap/Table";
+import Button from "react-bootstrap/Button";
+
 export const ProfilePage: React.FC<{}> = () => {
   const thisUser = useAppSelector((store) => store.sweet.currUser, equalFnForCurrUserDocChange);
   const navigate = useNavigate();
@@ -21,14 +24,48 @@ export const ProfilePage: React.FC<{}> = () => {
     }
   }, [navigate]);
 
+  if (!thisUser) {
+    return <p>You don't have access to this data</p>;
+  }
+
   return (
     <div className={style.ground}>
-      <div>{thisUser && `${thisUser?.email} (${thisUser.roles.admin ? "Admin" : "User"})`}</div>
+      <div className={style.mainBox}>
+        <Table striped bordered hover style={{ maxWidth: "808px" }}>
+          {/* <thead>
+          <tr>
+            <th>First name</th>
+            <th>Last name</th>
+            <th>Email</th>
+            <th>Role</th>
+          </tr>
+        </thead> */}
+          <tbody>
+            <tr>
+              <td className={style.label}>Fist name</td>
+              <td>{thisUser.firstName}</td>
+            </tr>
+            <tr>
+              <td className={style.label}>Last name</td>
+              <td>{thisUser.lastName}</td>
+            </tr>
+            <tr>
+              <td className={style.label}>Email</td>
+              <td>{thisUser.email}</td>
+            </tr>
+            <tr>
+              <td className={style.label}>Role</td>
+              <td>{thisUser.roles.admin ? "Admin" : "User"}</td>
+            </tr>
+          </tbody>
+        </Table>
 
-      <div>{thisUser && `${thisUser.firstName} ${thisUser.lastName}`}</div>
-
-      <div>
-        <button onClick={handleLogout}>Log out</button>
+        <div>
+          {/* <button onClick={handleLogout}>Log out</button> */}
+          <Button onClick={handleLogout} variant="outline-primary">
+            Log out
+          </Button>
+        </div>
       </div>
     </div>
   );
