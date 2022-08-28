8519b79b2b24ed574ea4e4bff074fcd11895452a
Levan Katsadze <leodevbro@gmail.com>
2022-08-28 14:58:04 +0400

    create function tryGetAllKeys

--- src/app/helper-functions.ts
+++ src/app/helper-functions.ts
@@ -2,6 +2,20 @@ import myLodash from "lodash";
 
 export const lodashObj = myLodash;
 
+// https://stackoverflow.com/questions/8024149/is-it-possible-to-get-the-non-enumerable-inherited-property-names-of-an-object
+export function tryGetAllKeys(obj: any) {
+  let keys: any[] = [];
+  // if primitive (primitives still have keys) skip the first iteration
+  if (!(obj instanceof Object)) {
+    obj = Object.getPrototypeOf(obj);
+  }
+  while (obj) {
+    keys = keys.concat(Reflect.ownKeys(obj));
+    obj = Object.getPrototypeOf(obj);
+  }
+  return keys;
+}
+
 export const weekDays = ["MO", "TU", "WE", "TH", "FR", "SA", "SU"];
 export const months = [
   "Jan",
